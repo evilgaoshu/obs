8d3a6131e21ff4f82c4156573dbbfd5cfcab91f0
evilgaoshu <evilgaoshu@gmail.com>
2022-11-06 09:08:07 +0800

    add cpu desc

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ One beach script for all beachmark
 
 ## Feature
 1. system info
+1.1 cpu(modles socket cpu virtual)
+2. system info
--- obs.sh
+++ obs.sh
@@ -6,3 +6,6 @@ echo "#########################"
 echo "#   one beach script    #"
 echo "#   version: 20221103   #"
 echo "#   current: ${date -s} ##
+echo "#########################"
+
+
