a5ed624ccd0693fb9780c1a679499803f789a8b0
evilgaoshu <44167236+evilgaoshu@users.noreply.github.com>
2022-12-14 18:10:13 +0800

    Update obs.sh

--- obs.sh
+++ obs.sh
@@ -12,8 +12,8 @@ echo date and module mode
 self-check(){
 # check disrabition
 # install relay
-   yum install curl wget iperf3 deidecode vim jq dd mtr dd awk sed 
-   apt install curl wget iperf3 deidecode vim jq dd mtr dd awk sed
+   yum install curl wget iperf3 deidecode vim jq dd mtr dd awk sed grep
+   apt install curl wget iperf3 deidecode vim jq dd mtr dd awk sed grep 
 }
 # self-check
 # install nessery soft
